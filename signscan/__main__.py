469349b0f30b44d9bf8d9f392f1cd5f4b883da49
inkarbegim <ik15@hw.ac.uk>
2019-11-08 19:39:31 +0000

    Update __main__.py
    Error in count_samples fixed

--- signscan/__main__.py
+++ signscan/__main__.py
@@ -13,6 +13,7 @@ import pandas
 from sklearn import naive_bayes
 from sklearn.base import BaseEstimator
 from sklearn.utils import column_or_1d
+from pprint import pprint
 
 label_mapping = [
     "limit_60", "limit_80", "limit_80_lifted",
@@ -77,6 +78,8 @@ def load_data(folder: str, *, shuffle=True) -> Tuple[pandas.DataFrame, YTrain, p
             elif "y_train" in file:
                 all_labels = pandas.read_csv(path.join(folder, file), names=["label"], header=0)
 
+    pprint(x_train);
+    # pprint(y_train);
     if shuffle:
         numpy.random.seed(seed=42)
         shuffled_indices = numpy.random.permutation(x_train.index)
@@ -118,8 +121,9 @@ def bayesian_classification(train: pandas.DataFrame, labels: pandas.DataFrame, n
     # generate heat map of correlation when dealing with the full image
     heat_map = numpy.reshape(success_probability, (-1, 48)) if len(success_probability) == 48 ** 2 else None
 
-    return BayesAnalysis(classifier, total_count, correct_count, top_features, heat_map, mistaken_photos)
-
+    result = BayesAnalysis(classifier, total_count, correct_count, top_features, heat_map, mistaken_photos)
+    # pprint(result.correct_predictions)
+    return result;
 
 @click.group()
 @click.argument("data_folder")
@@ -301,7 +305,7 @@ def count_samples(ctx):
 
     plt.scatter(
         x=[frame[frame.label==0].shape[0] for frame in y_train._asdict().values()],
-        y=[c.correct_predictions/c.total_predictions for c in label_classifiers.values()]
+        y=[c.correct_count/c.total_count for c in label_classifiers.values()]
     )
     plt.xlabel("number of samples")
     plt.ylabel("prediction accuracy")
