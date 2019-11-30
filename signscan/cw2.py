75b4f31cc2fd6e7c0d5fefba23cee0b182a0f0c5
Alexander Lyon <arlyon@me.com>
2019-11-30 14:13:43 +0000

    working commit

--- signscan/cw2.py
+++ signscan/cw2.py
@@ -1,22 +1,39 @@
+from enum import Enum
+
 import click
+import tensorflow
 
 from signscan.cli import signscan, load_data
 
 
-@signscan.command()
-@click.pass_context
-def cw2(ctx):
-    """
-    Naive Bayesian Classification and Deeper Analysis.
+class EnumType(click.Choice):
+    def __init__(self, enum):
+        self._enum = enum
+        super().__init__([e.value for e in enum])
 
-    - https://github.com/arlyon/dmml/issues/3
-    - https://github.com/arlyon/dmml/issues/4
-    """
+    def convert(self, value, param, ctx):
+        return self._enum(super().convert(value, param, ctx))
+
+
+class TrainingType(Enum):
+    CROSS_VALIDATION = "cross-validation"
+    TRAIN_TEST = "train-test"
+
+
+@signscan.command()
+@click.option("--train-type", type=EnumType(TrainingType), required=True)
+@click.option("--train-elements-move", type=int, default=0)
+@click.pass_context
+def neural_net(ctx, training_type: TrainingType, train_elements_move: int):
 
     print("loading data...")
     x_train, y_train, labels = load_data(ctx.obj["data_folder"], shuffle_seed=ctx.obj["seed"])
 
-    print("")
-    print("hello world")
+    if training_type is TrainingType.CROSS_VALIDATION:
 
+
+    elif training_type is TrainingType.TRAIN_TEST:
+        output = process_data(x_train, y_train, labels, train_elements_move)
+
+    linear_classifier = tensorflow.estimator.LinearClassifier()
 
