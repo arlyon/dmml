5b436564bfc3e7a4aaa0114471fb19e3fa36d778
Alexander Lyon <arlyon@me.com>
2019-11-30 14:59:40 +0000

    working commit

--- signscan/cw2.py
+++ signscan/cw2.py
@@ -23,17 +23,27 @@ class TrainingType(Enum):
 @signscan.command()
 @click.option("--train-type", type=EnumType(TrainingType), required=True)
 @click.option("--train-elements-move", type=int, default=0)
+@click.option("--model-dir", type=click.Path(file_okay=False, dir_okay=True, exists=True))
 @click.pass_context
-def neural_net(ctx, training_type: TrainingType, train_elements_move: int):
+def neural_net(ctx, train_type: TrainingType, train_elements_move: int, model_dir: str):
 
     print("loading data...")
     x_train, y_train, labels = load_data(ctx.obj["data_folder"], shuffle_seed=ctx.obj["seed"])
 
-    if training_type is TrainingType.CROSS_VALIDATION:
+    if train_type is TrainingType.CROSS_VALIDATION:
 
+        pass
 
-    elif training_type is TrainingType.TRAIN_TEST:
+    elif train_type is TrainingType.TRAIN_TEST:
         output = process_data(x_train, y_train, labels, train_elements_move)
 
-    linear_classifier = tensorflow.estimator.LinearClassifier()
+    dataset = tensorflow.data.Dataset.from_tensor_slices((dict(x_train), labels))
+    feature_columns = [tensorflow.feature_column.numeric_column(x, dtype=tensorflow.float32) for x in x_train.columns.values]
+    linear_est = tensorflow.estimator.LinearClassifier(
+        feature_columns=feature_columns,
+        model_dir=model_dir,
+    )
 
+    linear_est.train(dataset)
+
+    pass
