7b5738d26867a0b349e7bf9662932457cb19c96c
Regan Hay <regs.d.h@gmail.com>
2019-12-01 00:31:32 +0000

    randomforest

--- signscan/cw2.py
+++ signscan/cw2.py
@@ -1,6 +1,12 @@
 import click
+import graphviz
+import pydotplus
+import os
 
 from signscan.cli import signscan, load_data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+from sklearn.tree import export_graphviz
 
 
 @signscan.command()
@@ -14,9 +20,32 @@ def cw2(ctx):
     """
 
     print("loading data...")
-    x_train, y_train, labels = load_data(ctx.obj["data_folder"], shuffle_seed=ctx.obj["seed"])
-
-    print("")
-    print("hello world")
+    x_train, y_train, labels = load_data(ctx.obj["data_folder"])
 
 
+@signscan.command()
+@click.pass_context
+def randomforest(ctx):
+    """
+    Running Random Forest Classifier on the data
+    """
+
+    print("loading data...")
+    x_train, y_train, true_labels = load_data(ctx.obj["data_folder"])
+
+    print("Running Random Forest...")
+
+    clf = RandomForestClassifier(n_estimators=10)
+    clf.fit(x_train, true_labels)
+
+    print(clf.predict(x_train))
+    print(clf.decision_path(x_train))
+    print(clf.apply(x_train))
+    print(clf.score(x_train, true_labels))
+
+    i_tree = 0
+    for tree_in_forest in clf.estimators_:
+        dot_data = tree.export_graphviz(tree_in_forest, out_file=None)
+        graph = graphviz.Source(dot_data)
+        graph.render(f'{"tree_"}{i_tree}')
+        i_tree = i_tree + 1
